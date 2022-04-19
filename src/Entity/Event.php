86e0d35170a1277a89f05b76dfb0cfb6f6e9c503
Emmanuel SAUVAGE <emmanuel.sauvage@live.fr>
2022-04-19 11:35:50 +0200

    Create 2 entity Student & Event

---
+++ src/Entity/Event.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EventRepository::class)
+ */
+class Event
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $EventAt;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Student::class, inversedBy="events")
+     */
+    private $students;
+
+    public function __construct()
+    {
+        $this->students = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getEventAt(): ?\DateTimeImmutable
+    {
+        return $this->EventAt;
+    }
+
+    public function setEventAt(\DateTimeImmutable $EventAt): self
+    {
+        $this->EventAt = $EventAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Student>
+     */
+    public function getStudents(): Collection
+    {
+        return $this->students;
+    }
+
+    public function addStudent(Student $student): self
+    {
+        if (!$this->students->contains($student)) {
+            $this->students[] = $student;
+        }
+
+        return $this;
+    }
+
+    public function removeStudent(Student $student): self
+    {
+        $this->students->removeElement($student);
+
+        return $this;
+    }
+}
